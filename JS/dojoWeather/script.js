004ffac99ef563458cd8de5b7cb9968a3572dce9
Benjamin McBride <96863998+BenMcBride@users.noreply.github.com>
2023-03-23 07:54:25 -0700

    edited for mobile

--- JS/dojoWeather/script.js
+++ JS/dojoWeather/script.js
@@ -3,27 +3,22 @@ function hide(element) {
   hideMe.remove();
 }
 function convert(select) {
+  // set tempHigh and tempLow to select all the high and low temp elements
   var tempHigh = document.querySelectorAll(".high")
   var tempLow = document.querySelectorAll(".low")
+  // loop through each of the 4 high and low elements
   for(var i = 0; i < 4; i++){
+    // if selecting fahrenheit
     if(select.value == 'f'){
+      // change each high and low to convert to fahrenheit
       tempHigh[i].innerHTML = Math.round(tempHigh[i].innerHTML * 1.8 + 32);
       tempLow[i].innerHTML = Math.round(tempLow[i].innerHTML * 1.8 + 32);
-    } else {
+    } 
+    // else (they're selecting celsius)
+    else {
+      // convert each high and low to celsius
       tempHigh[i].innerHTML = Math.round((tempHigh[i].innerHTML - 32) / 1.8);
       tempLow[i].innerHTML = Math.round((tempLow[i].innerHTML - 32) / 1.8);
     }
   }
-  if(select.value == 'f'){
-    select.value = 'c';
-    select.innerHTML = 
-      "<option value='f'>ºF</option><option value='c'>ºC</option>"
-  }
-  else {
-    select.value = 'f'
-    select.innerHTML = 
-      "<option value='c'>ºC</option><option value='f'>ºF</option>"
-  }
 }
-
-// (32°F − 32) × 5/9 = 0°C
